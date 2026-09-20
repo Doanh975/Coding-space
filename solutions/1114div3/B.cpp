0d36dded9c0014316fcd5f727a4ba8d9072e3019
Doanh975 <tranhoangdoanh090705@gmail.com>
2026-09-20 16:33:46 +0700

    Create 1114div3

---
+++ solutions/1114div3/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    // code o day
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t; // bo comment neu bai co nhieu test case
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
